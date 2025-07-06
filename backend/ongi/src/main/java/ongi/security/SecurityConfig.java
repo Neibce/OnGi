dce5ba3a8f0b91cb2abbeb6ab51bbbb9216bf01b
oxomi <osm0071@naver.com>
2025-07-06 19:42:52 +0900

    feat: SecurityConfig에서 온도 API 경로(/api/temperatures/**)허용

--- backend/ongi/src/main/java/ongi/security/SecurityConfig.java
+++ backend/ongi/src/main/java/ongi/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
-                        auth -> auth.requestMatchers("/auth/**", "/users/exists").permitAll()
+                        auth -> auth.requestMatchers("/auth/**", "/users/exists", "/api/temperatures/**").permitAll()
                                 .anyRequest().authenticated())
                 .addFilterBefore(new AuthenticationFilter(tokenProvider),
                         UsernamePasswordAuthenticationFilter.class);
