b5508ef2024fb4d5e9c712820264625cace4d7ae
Yun-yeong Kim <143103801+surferwithus@users.noreply.github.com>
2025-07-16 23:16:39 +0900

    feat: light background 및 color 추가 (#23)

--- frontend/ongi/lib/core/app_colors.dart
+++ frontend/ongi/lib/core/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const ongiOrange = Color(0xFFFD6C01);
   static const ongiBlue = Color(0xFFBFECFF);
+  static const ongiLigntgrey = Color(0xFFF7F7F7);
 }
