3b81107fa311c5136584634e60511aa69a48060f
Junyoung Yang <y@jun0.dev>
2025-07-20 02:45:54 +0900

    feat: 운동 기록 화면 틀 완성

---
+++ frontend/ongi/lib/screens/health/exercise_record_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import '../../core/app_colors.dart';
+
+class ExerciseRecordScreen extends StatefulWidget {
+  const ExerciseRecordScreen({super.key});
+
+  @override
+  State<ExerciseRecordScreen> createState() => _ExerciseRecordScreenState();
+}
+
+class _ExerciseRecordScreenState extends State<ExerciseRecordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final circleSize = screenWidth * 1.56;
+
+    return Scaffold(
+      backgroundColor: AppColors.ongiLigntgrey,
+      body: SafeArea(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Transform.translate(
+                offset: Offset(0, -circleSize * 0.76),
+                child: OverflowBox(
+                  maxWidth: double.infinity,
+                  maxHeight: double.infinity,
+                  child: Container(
+                    width: circleSize,
+                    height: circleSize,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: AppColors.ongiOrange,
+                    ),
+                    child: Center(
+                      child: Padding(
+                        padding: EdgeInsets.only(top: circleSize),
+                        child: OverflowBox(
+                          maxHeight: double.infinity,
+                          child: Column(
+                            children: [
+                              const Text(
+                                '오늘 목표 운동 시간,',
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600,
+                                  height: 1,
+                                ),
+                              ),
+                              const Text(
+                                '다 채우셨나요?',
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.symmetric(
+                                  horizontal: 0,
+                                  vertical: 6,
+                                ),
+                                child: Image.asset(
+                                  'assets/images/exercise_record_title_logo.png',
+                                  width: circleSize * 0.3,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: circleSize * 0.3 + 60,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Text("여기부터 본문을 생성 ㅇ.ㅇ"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
