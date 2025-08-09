a4f6485806c348213c6fd6b5a00f8d10b86c909a
yunyeong-kim <dbsdud3272@naver.com>
2025-08-09 23:05:33 +0900

    feat: 프로필 아이콘 선택 화면 구현

--- frontend/ongi/lib/screens/signup/username_screen.dart
+++ frontend/ongi/lib/screens/signup/username_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ongi/core/app_colors.dart';
-import 'package:ongi/screens/signup/mode_screen.dart';
+import 'package:ongi/screens/signup/profilepic_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UsernameScreen extends StatefulWidget {
@@ -19,14 +19,15 @@ class _UsernameScreenState extends State<UsernameScreen> {
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10, top: 100, right: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(
-                    5, (index) => Container(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10, top: 100, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  5,
+                  (index) => Container(
                     width: 58,
                     height: 7.5,
                     margin: const EdgeInsets.symmetric(horizontal: 6),
@@ -37,122 +38,130 @@ class _UsernameScreenState extends State<UsernameScreen> {
                       borderRadius: BorderRadius.circular(4),
                     ),
                   ),
-                  ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30, right: 30, top: 40),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: const Text(
-                        '당신을',
-                        textAlign: TextAlign.right,
-                        style: TextStyle(
-                          fontSize: 60,
-                          fontWeight: FontWeight.w200,
-                          height: 1.2,
-                          color: AppColors.ongiOrange,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 80),
-                    const Text(
-                      '어떻게\n부르면\n좋을까요?',
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, top: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: const Text(
+                      '당신을',
+                      textAlign: TextAlign.right,
                       style: TextStyle(
                         fontSize: 60,
-                        fontWeight: FontWeight.w800,
+                        fontWeight: FontWeight.w200,
                         height: 1.2,
                         color: AppColors.ongiOrange,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 80),
+                  const Text(
+                    '어떻게\n부르면\n좋을까요?',
+                    style: TextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.w800,
+                      height: 1.2,
+                      color: AppColors.ongiOrange,
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 40, right: 40, top: 40),
-                child: TextField(
-                  controller: _usernameCtrl,
-                  keyboardType: TextInputType.text,
-                  style: const TextStyle(fontSize: 25, color: AppColors.ongiOrange),
-                  decoration: InputDecoration(
-                    hintText: '닉네임을 입력해주세요',
-                    hintStyle:
-                    TextStyle(color: Colors.grey),
-                    contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 24, vertical: 13),
-                    filled: true,
-                    fillColor: Colors.transparent,
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                      const BorderSide(color: AppColors.ongiOrange, width: 1),
-                      borderRadius: BorderRadius.circular(20),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 40, right: 40, top: 40),
+              child: TextField(
+                controller: _usernameCtrl,
+                keyboardType: TextInputType.text,
+                style: const TextStyle(
+                  fontSize: 25,
+                  color: AppColors.ongiOrange,
+                ),
+                decoration: InputDecoration(
+                  hintText: '닉네임을 입력해주세요',
+                  hintStyle: TextStyle(color: Colors.grey),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 13,
+                  ),
+                  filled: true,
+                  fillColor: Colors.transparent,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: AppColors.ongiOrange,
+                      width: 1,
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                      const BorderSide(color: AppColors.ongiOrange, width: 1),
-                      borderRadius: BorderRadius.circular(20),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: AppColors.ongiOrange,
+                      width: 1,
                     ),
+                    borderRadius: BorderRadius.circular(20),
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 40, right: 40, top: 20),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 65,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      backgroundColor: AppColors.ongiOrange,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 40, right: 40, top: 20),
+              child: SizedBox(
+                width: double.infinity,
+                height: 65,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    backgroundColor: AppColors.ongiOrange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                    onPressed: () async {
-                      final username = _usernameCtrl.text.trim();
-                      if (username.isEmpty) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: const Text(
-                              '이름을 입력해주세요.',
-                              style: TextStyle(color: AppColors.ongiOrange),
-                            ),
-                            backgroundColor: Colors.white,
-                            behavior: SnackBarBehavior.floating,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            duration: const Duration(seconds: 2),
+                  ),
+                  onPressed: () async {
+                    final username = _usernameCtrl.text.trim();
+                    if (username.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: const Text(
+                            '이름을 입력해주세요.',
+                            style: TextStyle(color: AppColors.ongiOrange),
                           ),
-                        );
-                        return;
-                      }
-
-                      final prefs = await SharedPreferences.getInstance();
-                      await prefs.setString('signup_username', username);
-
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ModeScreen(username),
+                          backgroundColor: Colors.white,
+                          behavior: SnackBarBehavior.floating,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          duration: const Duration(seconds: 2),
                         ),
                       );
-                    },
-                    child: const Text(
-                      '계속하기',
-                      style: TextStyle(
-                        fontSize: 33,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.white,
+                      return;
+                    }
+
+                    final prefs = await SharedPreferences.getInstance();
+                    await prefs.setString('signup_username', username);
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProfilepicScreen(username),
                       ),
+                    );
+                  },
+                  child: const Text(
+                    '계속하기',
+                    style: TextStyle(
+                      fontSize: 33,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ),
-            ]
+            ),
+          ],
         ),
       ),
     );
