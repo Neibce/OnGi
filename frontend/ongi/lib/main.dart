e0f2cd60027fc73414646021d72fd52c8af18ffd
Yun-yeong Kim <143103801+surferwithus@users.noreply.github.com>
2025-08-12 18:38:07 +0900

    feat: splash 화면 구현 및 기존 native splash 제거 (#117)

--- frontend/ongi/lib/main.dart
+++ frontend/ongi/lib/main.dart
@@ -1,18 +1,14 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:ongi/screens/login/login_pw_screen.dart';
-import 'package:ongi/screens/start_screen.dart';
+import 'package:ongi/screens/splash_screen.dart';
 import 'package:ongi/screens/signup/password_screen.dart';
-import 'package:ongi/screens/bottom_nav.dart';
-import 'package:ongi/utils/prefs_manager.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting();
 
   // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -30,31 +26,8 @@ void main() async {
   runApp(const OngiApp());
 }
 
-class OngiApp extends StatefulWidget {
+class OngiApp extends StatelessWidget {
   const OngiApp({super.key});
-
-  @override
-  State<OngiApp> createState() => _OngiAppState();
-}
-
-class _OngiAppState extends State<OngiApp> {
-  Widget _homeWidget = const StartScreen();
-
-  @override
-  void initState() {
-    super.initState();
-    _initializeApp();
-  }
-
-  Future<void> _initializeApp() async {
-    final hasToken = await PrefsManager.hasAccessToken();
-
-    setState(() {
-      _homeWidget = hasToken ? const BottomNavScreen() : const StartScreen();
-    });
-
-    FlutterNativeSplash.remove();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,9 +35,7 @@ class _OngiAppState extends State<OngiApp> {
       title: 'Ongi',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true, fontFamily: 'Pretendard'),
-
-      home: _homeWidget,
-
+      home: const SplashScreen(),
       routes: {
         '/login': (context) => const LoginPwScreen(),
         '/signup': (context) => const PasswordScreen(),
