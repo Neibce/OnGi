8ec5de8819a0d65948f1c5ff3db46584a7e71e7a
Junyoung Yang <y@jun0.dev>
2025-08-13 19:30:00 +0900

    feat: 유저 fcm 토큰 업로드 기능 추가

--- backend/ongi/src/main/java/ongi/user/service/UserService.java
+++ backend/ongi/src/main/java/ongi/user/service/UserService.java
@@ -1,6 +1,9 @@
 package ongi.user.service;
 
 import lombok.RequiredArgsConstructor;
+import ongi.user.dto.FcmTokenUpdateRequestDto;
+import ongi.user.entity.User;
+import ongi.user.entity.UserFcmToken;
 import ongi.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,8 +14,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserFcmTokenRepository userFcmTokenRepository;
 
     public boolean getUserExists(String email) {
         return userRepository.existsByEmail(email);
+    }
+
+    @Transactional
+    public void updateFcmToken(User user, FcmTokenUpdateRequestDto requestDto) {
+        UserFcmToken userFcmToken = userFcmTokenRepository.findByUser(user).orElse(
+                UserFcmToken.builder().user(user).token(requestDto.fcmToken()).build());
+
+        userFcmTokenRepository.save(userFcmToken);
     }
 }
