d7dbe0becb601d87ac5d9037f8a479aa07e277b4
Junyoung Yang <y@jun0.dev>
2025-08-13 19:30:15 +0900

    feat: 유저 fcm 토큰 업로드 기능 추가 (#128)

---
+++ backend/ongi/src/main/java/ongi/user/entity/UserFcmToken.java
@@ -0,0 +1,34 @@
+package ongi.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ongi.common.entity.BaseEntity;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class UserFcmToken extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @OneToOne(optional = false, fetch = FetchType.LAZY)
+    User user;
+
+    @Column(nullable = false, updatable = false)
+    String token;
+}
