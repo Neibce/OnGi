1478080c3299464ea44f50a4f9f3f579172d2190
Junyoung Yang <y@jun0.dev>
2025-08-18 16:02:16 +0900

    fix: url 수정

--- backend/ongi/src/main/java/ongi/pill/controller/PillController.java
+++ backend/ongi/src/main/java/ongi/pill/controller/PillController.java
@@ -16,6 +16,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,10 +45,10 @@ public class PillController {
         return ResponseEntity.ok(pillInfo);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{pillId}")
     public ResponseEntity<PillInfo> deletePill(
             @AuthenticationPrincipal CustomUserDetails userDetails,
-            @RequestParam Long pillId) {
+            @PathVariable Long pillId) {
         pillService.deletePill(userDetails.getUser(), pillId);
         return ResponseEntity.ok().build();
     }
