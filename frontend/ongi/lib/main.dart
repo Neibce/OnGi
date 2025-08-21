f7ff39d3687addbb91681f39bb86536b2e47651a
Junyoung Yang <y@jun0.dev>
2025-08-22 05:22:12 +0900

    feat: fcm 알림 약 복용 체크용 커스텀 액션 추가

--- frontend/ongi/lib/main.dart
+++ frontend/ongi/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:ongi/screens/login/login_pw_screen.dart';
 import 'package:ongi/screens/splash_screen.dart';
@@ -8,6 +9,8 @@ import 'package:ongi/utils/prefs_manager.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+
+final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,8 +28,37 @@ void main() async {
     provisional: false,
     sound: true,
   );
+
+  messaging.setForegroundNotificationPresentationOptions(
+    alert: true, badge: true, sound: true,
+  );
   
   _initializeFCMIfLoggedIn();
+
+  const categoryId = 'PILL_TAKE_REMINDER';
+  final iosInit = DarwinInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+    notificationCategories: [
+      DarwinNotificationCategory(
+        categoryId,
+        actions: [
+          DarwinNotificationAction.plain('ACCEPT', '복용 완료!'),
+        ],
+      ),
+    ],
+  );
+  final initSettings = InitializationSettings(iOS: iosInit);
+  await flutterLocalNotificationsPlugin.initialize(
+    initSettings,
+    onDidReceiveNotificationResponse: (resp) {
+      switch (resp.actionId) {
+        case 'ACCEPT':
+          break;
+      }
+    }
+  );
 
   runApp(const OngiApp());
 }
