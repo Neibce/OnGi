723e6196337af856df2a9524ccac9a13e6395783
Junyoung Yang <y@jun0.dev>
2025-08-21 18:10:07 +0900

    refactor: google admin sdk 인증서 getApplicationDefault 참조하도록 변경 (#163)

--- backend/ongi/src/main/java/ongi/firebase/FirebaseConfig.java
+++ backend/ongi/src/main/java/ongi/firebase/FirebaseConfig.java
@@ -1,14 +1,21 @@
 package ongi.firebase;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import java.io.IOException;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
-    public void initFirebase() {
-        FirebaseApp.initializeApp();
+    public void initFirebase() throws IOException {
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .build();
+
+        FirebaseApp.initializeApp(options);
     }
 }
