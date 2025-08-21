b643e6a6f480e862c3295aa508950ed18a864da9
Junyoung Yang <y@jun0.dev>
2025-08-21 15:53:51 +0900

    fix: firebase 설정 파일 위치 변경

--- backend/ongi/src/main/java/ongi/firebase/FirebaseConfig.java
+++ backend/ongi/src/main/java/ongi/firebase/FirebaseConfig.java
@@ -1,14 +1,27 @@
 package ongi.firebase;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("file:/var/secrets/ongi-3841c-firebase-adminsdk-fbsvc-e0b6d1072f")
+    private Resource resource;
+
     @PostConstruct
-    public void initFirebase() {
-        FirebaseApp.initializeApp();
+    public void initFirebase() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        FirebaseApp.initializeApp(options);
     }
 }
